4a1fb0084f6da89f990227889c7628d207f1ff45
SenkeSem <sen9morf@gmail.com>
2024-10-22 16:41:35 +0300

    added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
-import './App.css';
-import { DatePicker } from 'antd';
+import { Layout, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+import {
+  HomeOutlined,
+  LoadingOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+} from '@ant-design/icons';
+import { Space } from 'antd';
 
 function App() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
-    <>
-      <h1>Hello world!</h1>
-      <DatePicker />
-    </>
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}></Header>
+      <Content
+        style={{
+          padding: '20px',
+        }}>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}>
+          <Space>
+            <HomeOutlined style={{ fontSize: '50px', color: '#08c' }} />
+            <SettingFilled />
+            <SmileOutlined />
+            <SyncOutlined spin />
+            <SmileOutlined rotate={180} />
+            <LoadingOutlined />
+          </Space>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
 
