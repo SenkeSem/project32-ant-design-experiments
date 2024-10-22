5a5eefc9cc1283635c40bcb034e2d9b575a20d68
SenkeSem <sen9morf@gmail.com>
2024-10-22 15:21:30 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <DatePicker />
+    </>
+  );
+}
+
+export default App;
